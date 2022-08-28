01591b5702f1e062ae0aae35eafb7cbc3face509
LPandorf <leticiapandorf@gmail.com>
2022-08-28 18:53:45 -0300

    ffe

---
+++ script.js
@@ -0,0 +1,141 @@
+const urlAPI="";
+let ultimaHora;
+let nick;
+
+let tipoDeMsg ="message";
+let destinatario="Todos";
+
+//função para selecionar quem recebe a msg
+function selecionarUsuario(usuarioTo, elemento){
+    const selecionado = document.querySelector(".contatos .selecionado");
+    if(selecionado){
+        selecionado.classList.remove("selecionado");
+    }
+    elemento.classList.add("selecionado");
+    destinatario=usuarioTo;
+    //mexe menu lateral
+    toggleMenu();
+}
+
+//função para mostar os usuarios
+function mostrarUsuarios(resposta){
+    console.log(resposta.data);
+    const contatos = document.querySelector(".contatos");
+    for(let i=0; i<resposta.data.length; i++){
+        const usuarioTemplate =`
+            <li class="visivel-publico" onclick="selectedUsuario('${resposta.data[i].name}', this)">
+                <ion-icon name="person-circle"></ion-icon>
+                <span class="nome">${resposta.data[i].name}</span>
+                <ion-icon class="check" name="checkmark-outline"></ion-icon>
+            </li>`;
+        contatos.innerHTML+=usuarioTemplate;
+    }
+}
+
+//função para carregar os usuários
+function carregarUsuarios(){
+    console.log("CARREGAR USUARIOS");
+    //promessa da função carregar, escopo local
+    const promise = axios.get(`${urlAPI}/participants`);
+    promise.then(mostrar); 
+    //callback
+}
+
+//função para ir até o fim das msg, tipo o whats q mostra as mais novas 
+function whats(ultimaMsg){
+    if(ultimaMsg !== ultimaHora){
+        document
+            .querySelector(".chat li:last-child")
+            .scrollIntoView();
+        ultimaHora=ultimaMsg;
+    }
+}
+
+//função para ver as mensagens privadas
+function visualizarPvd(tMsg){
+    if(tMsg.type==="private_message" && (tMsg.from===nick || tMsg.to===nick)){
+        return true;
+    }
+    return false;
+}
+
+//função para carregar as mensagens
+function carregarMsg(){
+    if(nick !== undefined){
+        const promise = axios.get(´${urlAPI}/messages´);
+        //promete a função q mostra as msgs
+        promise.then(mostrarMsg);
+    }
+}
+
+//função para entrar na sala
+function entrar(){
+    //como não fiz o bonus (pelo menos n ainda) o nome do usuario é pego por um prompt
+    nick = prompt("Escreva seu nome");
+    const promise=axios.post(`${urlAPI}/participants`, {name: nick});
+    promise.catch(errorEntrar);
+    promise.then(carregarMsg);
+}
+
+//função para continuar conectado
+function continuarConectado(){
+    if(nick){
+        console.log("status");
+        axios.post(`${urlAPI}/status`, {name: nick});
+    }
+}
+
+//função de erro ao entrar na sala
+function erroEntrar(){
+    console.log("ERRO");
+    nick=undefined;
+}
+
+//função para mostrar as mensagens, colocar no dominio
+function mostrarMsg(resposta){
+    console.log("mostrarMsg");
+    let containerChat = document.querySelector(".chat");
+    containerChat.innerHTML="";
+    for(let i=0; i<resposta.data.length; i++){
+        const tMsg = resposta.data[i];
+        //para mensagens privadas
+        if(visualizarPvd(tMsg)){
+            containerChat.innerHTML+= `
+                    <li class="privada">
+                        <span class="hora">(${tMsg.time})</span>
+                            <strong>${tMsg.from}</strong>
+                                <span> reservadamente para </span>
+                            <strong>${tMsg.to}: </strong>
+                        <span>${tMsg.text}</span>
+                    </li>
+                `;
+        }
+        //para mensagens publicas
+        if(tMsg.type==="message"){
+            containerChat.innerHTML+= `
+                    <li class="publica">
+                        <span class="hora">(${tMsg.time})</span>
+                            <strong>${tMsg.from}</strong>
+                                <span> para </span>
+                            <strong>${tMsg.to}: </strong>
+                        <span>${tMsg.text}</span>
+                    </li>            
+                `;
+        }
+        //para mensagens de status
+        if(tMsg.type==="status"){
+            containerChat.innerHTML+= `
+                    <li class="entradaesaida">
+                        <span class="hora">(${tMsg.time})</span>
+                            <strong>${tMsg.from}</strong>
+                                <span> para </span>
+                            <strong>${tMsg.to}: </strong>
+                        <span>${tMsg.text}</span>
+                    </li>
+                `;
+        }
+    }
+    const ultimaMsg = resposta.data[resposta.data.length-1].time;
+    whats(ultimaMsg);
+}
+
